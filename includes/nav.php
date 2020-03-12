8511b71c043faf9b430a8e24e1f0e8b60a254937
Andrew Von Haden <andrewvonhaden@gmail.com>
2020-03-12 14:09:26 -0500

    Added extra sass files

--- includes/nav.php
+++ includes/nav.php
@@ -1,1 +1,33 @@
 <!---------------------- NAVBAR ---------------------->
+<div class="uk-navbar-container uk-sticky" uk-sticky>
+    <div class="uk-container uk-container-expand">
+        <nav class="uk-navbar">
+
+            <!-- LEFT -->
+            <div class="uk-navbar-left">
+                <a href="" class="uk-navbar-item uk-logo">Andrew Von Haden</a>
+            </div>
+
+            <!-- RIGHT -->
+            <div class="uk-navbar-right">
+                <ul class="uk-navbar-nav uk-visible">
+                    <li>
+                        <a href="">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="">Link 3</a>
+                    </li>
+                </ul>
+                <a uk-navbar-toggle-icon=""
+                   href=""
+                   uk-toggle=""
+                   class="uk-navbar-toggle uk-hidden@m uk-icon uk-navbar-toggle-icon">
+                </a>
+            </div>
+
+        </nav>
+    </div>
+</div>
