204e29096765a14153336e5cd008dbfbfe3c3b66
Andrew Von Haden <andrewvonhaden@gmail.com>
2020-03-29 16:35:25 -0500

    Formatted mobile and desktop footers

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,40 @@
 <!---------------------- FOOTER ---------------------->
-<footer>
+<footer class="uk-section footer">
+    <div class="uk-container">
 
+        <!-- LEFT -->
+        <div class="left uk-margin-remove-bottom uk-flex">
+            <div class="logo">
+                <img src="img/AVH-Logo.svg" height="50" alt="logo" uk-svg>
+            </div>
+            <div>
+                <h5>Andrew Von Haden</h5>
+                <p class="uk-margin-remove-bottom">andrewvonhaden@gmail.com</p>
+            </div>
+        </div>
+
+        <!-- RIGHT -->
+        <div class="right">
+            <div class="social-links">
+                <a href="#">
+                    <i class="fab fa-github"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+    </div>
 </footer>
+
+
+<!---------------------- COPYRIGHT ---------------------->
+<div class="copyright uk-section">
+    <div class="uk-container">
+        <p class="uk-text-center">Handcrafted by Andrew Von Haden © twentytwenty</p>
+        <p class="uk-text-center">Made with <img src="img/uikit.svg" alt="uikit." height="20" uk-svg></p>
+    </div>
+</div>
