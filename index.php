363e82ef312c66ef4880603107fa15a3084970d8
Andrew Von Haden <andrewvonhaden@gmail.com>
2020-03-29 15:27:42 -0500

    Updated content on index.php to match index.html

--- index.php
+++ index.php
@@ -1,64 +1,131 @@
 <?php
 // set page title
-$pageTitle = '';
+$pageTitle = 'Andrew Von Haden';
 
 // set meta description
 $metaDescription = '';
 
 // includes
 include "includes/head.php";
-include "includes/nav.php";
+include "includes/nav-transparent.php";
 ?>
 
 
-<div class="uk-height-large uk-background-cover uk-light uk-flex"
-     uk-parallax="bgy: -100"
-     style="background-image: url('img/cover-bg.jpg');">
+<!-- BANNER -->
+<div class="banner uk-background-fixed uk-background-cover uk-background-center-center uk-light uk-flex"
+     uk-height-viewport="offset-top: true"
+     style="background-image: url('img/cover-bg.jpg');"
+>
     <div class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">
-        <img src="img/vonhaden-logo-white.png" alt="logo">
-        <h1>Headline</h1>
-        <p>I am a designer and photographer located in Southeast Wisconsin. I have a passion for all things visual and I channel that into all that I do.</p>
+        <img src="img/AVH-Logo.svg" height="200" alt="logo" class="uk-animation-scale-up" uk-svg>
+        <h2 class="uk-animation-fade">I am a designer and photographer located in Southeast Wisconsin.</h2>
     </div>
 </div>
 
-<!-- Placeholder Section -->
-<div class="uk-section uk-section-muted">
+
+<!-- Who am I -->
+<div class="uk-section bg-grey-white">
     <div class="uk-container">
-        <h3>Section</h3>
-        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
+        <h2>Who am I?</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+</div>
+
+
+<!-- Web -->
+<div class="portfolio-showcase web-showcase uk-section">
+    <div class="uk-container">
+
+        <h2>Web</h2>
+
+        <div class="links uk-child-width-1-3@m" uk-grid>
+
+            <a href="#">
+                <div class="link-box-outer uk-height-medium">
+                    <div class="link-box-inner uk-background-cover" data-src="img/cover-bg.jpg" uk-img></div>
+                    <div class="link-text">Link Text</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="link-box-outer uk-height-medium">
+                    <div class="link-box-inner uk-background-cover" data-src="img/cover-bg.jpg" uk-img></div>
+                    <div class="link-text">Link Text</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="link-box-outer uk-height-medium">
+                    <div class="link-box-inner uk-background-cover" data-src="img/cover-bg.jpg" uk-img></div>
+                    <div class="link-text">Link Text</div>
+                </div>
+            </a>
+
         </div>
+
+        <a href="#" class="uk-margin-top uk-button uk-button-default uk-align-right@m button button-teal">See More</a>
+
     </div>
 </div>
 
-<div class="uk-section">
+
+
+<!-- Testimonials -->
+<div class="testimonials uk-section bg-grey-white">
     <div class="uk-container">
-        <h2>This is some filler</h2>
-        <p>How about that filler</p>
+        <h2 class="uk-text-center">Testimonials</h2>
+        <div class="uk-position-relative uk-visible-toggle"
+             tabindex="-1"
+             uk-slideshow="min-height: 300; max-height: 500">
+
+            <ul class="uk-slideshow-items">
+
+                <li class="testimonial-slide">
+                    <div class="testimonial-slide-content uk-position-center">
+                        <img class="uk-align-center uk-border-circle" data-src="img/adam.jpg" width="100" height="100" alt="" uk-img>
+                        <h3 class="uk-text-center">Adam Derus</h3>
+                        <h4 class="uk-text-center">Senior Strategic Marketer, Direct Supply</h4>
+                        <p class="uk-text-center">"I worked with Andrew quite a bit in my role as strategic and content direction, while Andrew's role was to make it come to life. He was always great with creative ideas and there was no project too large for his creative eye. It was also great being able to have him consult on projects I was doing."</p>
+                    </div>
+                </li>
+                <li class="testimonial-slide">
+                    <div class="testimonial-slide-content uk-position-center">
+                        <img class="uk-align-center uk-border-circle" data-src="img/adam.jpg" width="100" height="100" alt="" uk-img>
+                        <h3 class="uk-text-center">Adam Derus</h3>
+                        <h4 class="uk-text-center">Senior Strategic Marketer, Direct Supply</h4>
+                        <p class="uk-text-center">"I worked with Andrew quite a bit in my role as strategic and content direction, while Andrew's role was to make it come to life. He was always great with creative ideas and there was no project too large for his creative eye. It was also great being able to have him consult on projects I was doing."</p>
+                    </div>
+                </li>
+                <li class="testimonial-slide">
+                    <div class="testimonial-slide-content uk-position-center">
+                        <img class="uk-align-center uk-border-circle" data-src="img/adam.jpg" width="100" height="100" alt="" uk-img>
+                        <h3 class="uk-text-center">Adam Derus</h3>
+                        <h4 class="uk-text-center">Senior Strategic Marketer, Direct Supply</h4>
+                        <p class="uk-text-center">"I worked with Andrew quite a bit in my role as strategic and content direction, while Andrew's role was to make it come to life. He was always great with creative ideas and there was no project too large for his creative eye. It was also great being able to have him consult on projects I was doing."</p>
+                    </div>
+                </li>
+
+
+            </ul>
+
+            <a class="uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+            <a class="uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+
+            <ul class="uk-slideshow-nav uk-dotnav uk-flex-center uk-margin"></ul>
+        </div>
+
+
     </div>
 </div>
 
-<div class="uk-section uk-section-muted">
+
+<!-- Connect -->
+<div class="connect uk-section">
     <div class="uk-container">
-        <h3>Section</h3>
-        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
+        <img src="img/paper-plane.svg" height="50"  class="uk-align-center" uk-svg>
+        <h2 class="uk-text-center">Interested in Connecting?</h2>
+        <div class="uk-flex uk-flex-center">
+            <a href="#" class="uk-button uk-button-default button button-teal">Send Email</a>
         </div>
     </div>
 </div>
