3fc01ce8923158a080f90bd480e681eac1784368
Andrew Von Haden <andrewvonhaden@gmail.com>
2020-05-05 18:14:44 -0500

    Refactored navbar for simplicity

---
+++ includes/nav-content.php
@@ -0,0 +1,29 @@
+<nav class="uk-navbar">
+
+    <!-- LEFT -->
+    <div class="uk-navbar-left">
+        <a href="../" class="uk-navbar-item uk-logo">
+            <img src="img/AVH-Logo.svg" height="40" alt="logo" uk-svg>
+            Andrew Von Haden
+        </a>
+    </div>
+
+    <!-- RIGHT -->
+    <div class="uk-navbar-right">
+        <a uk-navbar-toggle-icon
+           class="uk-navbar-toggle uk-hidden@m uk-icon">
+        </a>
+        <ul class="nav-links uk-navbar-nav">
+            <li>
+                <a href="web.php">Web</a>
+            </li>
+            <li>
+                <a href="design.php">Design</a>
+            </li>
+            <li>
+                <a href="">About</a>
+            </li>
+        </ul>
+    </div>
+
+</nav>
--- includes/nav-transparent.php
+++ includes/nav-transparent.php
@@ -6,35 +6,7 @@
                 cls-inactive: uk-navbar-transparent;
                 top: 0">
         <div class="uk-container uk-container-expand">
-            <nav class="uk-navbar">
-
-                <!-- LEFT -->
-                <div class="uk-navbar-left">
-                    <a href="../" class="uk-navbar-item uk-logo">
-                        <img src="img/AVH-Logo.svg" height="40" alt="logo" uk-svg>
-                        Andrew Von Haden
-                    </a>
-                </div>
-
-                <!-- RIGHT -->
-                <div class="uk-navbar-right">
-                    <a uk-navbar-toggle-icon
-                       class="uk-navbar-toggle uk-hidden@m uk-icon">
-                    </a>
-                    <ul class="nav-links uk-navbar-nav">
-                        <li>
-                            <a href="web.php">Web</a>
-                        </li>
-                        <li>
-                            <a href="design.php">Design</a>
-                        </li>
-                        <li>
-                            <a href="">About</a>
-                        </li>
-                    </ul>
-                </div>
-
-            </nav>
+            <?php include "nav-content.php" ?>
         </div>
     </div>
 
