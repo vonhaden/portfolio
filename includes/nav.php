a5ba15c935d12f5d1ebe3a8248b0a7cf8cbe3aa9
Andrew Von Haden <andrewvonhaden@gmail.com>
2020-05-05 18:12:51 -0500

    Navbars refactored for simplicity

--- includes/nav.php
+++ includes/nav.php
@@ -1,34 +1,6 @@
 <!---------------------- NAVBAR ---------------------->
 <div class="nav uk-navbar-container uk-sticky uk-navbar-grey" uk-sticky>
     <div class="uk-container uk-container-expand">
-        <nav class="uk-navbar">
-
-            <!-- LEFT -->
-            <div class="uk-navbar-left">
-                <a href="../" class="uk-navbar-item uk-logo">
-                    <img src="img/AVH-Logo.svg" height="40" alt="logo" uk-svg>
-                    Andrew Von Haden
-                </a>
-            </div>
-
-            <!-- RIGHT -->
-            <div class="uk-navbar-right">
-                <a uk-navbar-toggle-icon
-                   class="uk-navbar-toggle uk-hidden@m uk-icon">
-                </a>
-                <ul class="nav-links uk-navbar-nav">
-                    <li>
-                        <a href="">Web</a>
-                    </li>
-                    <li>
-                        <a href="">Design</a>
-                    </li>
-                    <li>
-                        <a href="">About</a>
-                    </li>
-                </ul>
-            </div>
-
-        </nav>
+        <?php include "nav-content.php" ?>
     </div>
 </div>
